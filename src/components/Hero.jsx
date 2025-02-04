e60b35b1fa62c4b2eb91ee1a25b50d76ecd64a57
Bana Dawit <banadawithund@gmail.com>
2025-02-04 15:05:24 +0300

    Add Newsletter component and some styles in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const FINANCING_TEXT = "Fast, flexible financing for businesses";
 
 const Hero = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center text-white">
+    <div className="w-full min-h-screen flex items-center justify-center text-white">
       <div className="max-w-[800px] mx-auto text-center flex flex-col justify-center px-4">
         <p className="text-[#00df9a] font-bold uppercase">Growing with Data Analytics</p>
         <h1 className="font-bold text-4xl md:text-7xl sm:text-6xl my-4">Grow with data.</h1>
@@ -25,6 +25,7 @@ const Hero = () => {
         <p className="md:text-2xl text-lg font-semibold text-gray-300 mt-4">
             Monitor your data analytics to increase revenue for B2B, BTC & SaaS platforms.
         </p>
+        <button className="bg-[#00df9a] text-black w-44 rounded-md font-medium my-6 mx-auto py-3">Get Started</button>
 
       </div>
     </div>
---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Newsletter = () => {
+  return (
+    <div className="w-full py-16 text-white">
+      <div className="max-w-[1240px] mx-auto grid lg-grid-cols-3">
+        <div>
+          <h1>Want tips & tricks to optimize your flow?</h1>
+          <p>Sign up to our newsletter and stay up to date</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Newsletter;
